f6fa9349f6e159f8dbbcff56ff6e036302aeab15
Jacob Hammack <jacob.hammack@hammackj.com>
2010-08-11 14:39:29 -0500

    dded -w and -u options for url scanning and file uploads respectively.

--- virustotal.rb
+++ virustotal.rb
@@ -9,23 +9,30 @@
 #06-10-2010: JPH - Added a check for the invalid hash error that seems to happen on some MD5 hashes
 #06-10-2010: JPH - Added a timer between hash lookups from files
 #08-10-2010: JPH - Modified to use the new virustotal api, the code has been simplified.
+#08-11-2010: JPH - Added file upload option -u, so that files can be uploaded and waits for results
+#08-11-2010: JPH - Added a url scan option -w, so that urls can be scanned using the new api
 
 require 'rubygems'
-require 'json'
+require 'json' 					#sudo gem install json
 require 'optparse'
-require "net/http"
-require "net/https"
-require "uri"
+#require "net/http"
+#require "net/https"
+#require "uri"
+
+require "rest_client" 	#sudo gem install rest-client
 
 
 $options = {}
 $options["xml"] = false
-$version = "1.4"
+$version = "1.4b"
+$timeout = 16
 
 $api_key = "<INSERT KEY HERE>"
 
 files = Array.new
 hashes = Array.new
+uploads = Array.new
+sites = Array.new
 
 # Displays the results of the virustotal query
 #
@@ -51,35 +58,28 @@ def fetch_results_from_file(file)
   
   inputHashes.each { |line|
       line.chomp!
-      puts "[*] Looking up hash #{line}" unless $options["debug"] != true
-      result = fetch_results_from_hash(line)
-      sleep 5 #So we do not DOS virustotal.com we wait 5 seconds between each query
+      result = fetch_results_from_hash(line)   
       display_output(result)
+			sleep $timeout #So we do not DOS virustotal.com we wait 5 seconds between each query
     }
 end
 
-# Fetch results from virustotal using a specific hash
+# Fetches a hash or vt timestamped hash results from vt
 #
-def fetch_results_from_hash(hash)
+def rest_fetch_results_from_hash(hash)
 	results = Array.new
 	hash = hash.chomp
 	
+	if hash.include?('-')
+		hash = hash.split('-')[0]
+	end
+	
 	begin
-		puts "[*] Attempting to query hash #{hash}"  unless $options["debug"] != true
+		puts "[*] Querying hash #{hash}"  unless $options["debug"] != true
 		
-		uri = URI.parse('https://www.virustotal.com/api/get_file_report.json')
-		params = {:resource => hash, :key => $api_key }
+		response = RestClient.post 'https://www.virustotal.com/api/get_file_report.json', { :resource => hash, :key => $api_key }
 
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-		request = Net::HTTP::Post.new(uri.path)
-		request.set_form_data(params)
-		
-		req = Net::HTTP::Post.new(uri.path + '?' + request.body)
-		response = http.request(req)		
-		result = JSON.parse(response.read_body)
+		result = JSON.parse(response)
 		
 		if result["result"] == 0
 			fres = Hash.new
@@ -106,8 +106,120 @@ def fetch_results_from_hash(hash)
 		end
 	rescue Exception => e		
 		puts e.message
-		STDERR.puts "[!] An error has occured. Retrying %s\n", hash
-		sleep 5 #So we do not DOS virustotal.com we wait 5 seconds between each query
+		STDERR.puts "[!] An error has occured. Retrying #{hash}\n"
+		sleep $timeout #So we do not DOS virustotal.com we wait 5 seconds between each query
+		retry
+	end
+
+	if results.length == 0
+			fres = Hash.new
+			fres['hash'] = hash
+			fres['scanner'] = '-'
+			fres['version'] = '-'
+			fres['date'] = '-'
+			fres['result'] = "No Antivirus Results"
+			
+			results.push fres	
+	end
+	
+	return results
+end
+
+
+# Fetches a hash or vt timestamped hash results from vt
+#
+def rest_fetch_results_from_url(url)
+	results = Array.new
+	url = url.chomp
+	
+	begin
+		puts "[*] Querying url #{url}"  unless $options["debug"] != true
+		
+		response = RestClient.post 'https://www.virustotal.com/api/get_url_report.json', { :resource => url, :key => $api_key }
+		result = JSON.parse(response)
+
+		if result["result"] == 0
+			fres = Hash.new
+			fres['hash'] = url
+			fres['scanner'] = '-'
+			fres['version'] = '-'
+			fres['date'] = '-'
+			fres['result'] = "Url Not Found"
+			
+			results.push fres
+		else		
+			result["report"][1].each do |scanner, res|
+				if res != ''
+					fres = Hash.new
+					fres['hash'] = url
+					fres['scanner'] = scanner
+					fres['version'] = '-'
+					fres['date'] = '-'
+					fres['result'] = res
+					
+					results.push fres
+				end
+			end
+		end
+	rescue Exception => e		
+		puts e.message
+		STDERR.puts "[!] An error has occured. Retrying #{url}\n"
+		sleep $timeout #So we do not DOS virustotal.com we wait 5 seconds between each query
+		retry
+	end
+
+	if results.length == 0
+			fres = Hash.new
+			fres['hash'] = hash
+			fres['scanner'] = '-'
+			fres['version'] = '-'
+			fres['date'] = '-'
+			fres['result'] = "No Scanner Results"
+			
+			results.push fres	
+	end
+
+	return results
+end
+
+# Fetch results from virustotal using a specific hash
+#
+def rest_upload_and_fetch_results_from_file(file)
+	results = Array.new
+	file = file.chomp
+	
+	begin
+		puts "[*] Attempting to upload file #{file}"  unless $options["debug"] != true
+		
+		response = RestClient.post 'https://www.virustotal.com/api/scan_file.json', { :key => $api_key, :file => File.new(file, 'rb') }
+		result = JSON.parse(response)
+
+		puts "[*] File #{file} uploaded, waiting for results this could take several minutes..."  unless $options["debug"] != true
+
+		if result['result']	== 1
+			results = rest_fetch_results_from_hash(result['scan_id'])
+
+			while results[0]['result'] == "Hash Not Found"
+				puts "[*] File has not been analyized yet, waiting 60 seconds to try again"  unless $options["debug"] != true
+				sleep 60				
+				results = rest_fetch_results_from_hash(result['scan_id'])
+			end
+		elsif result['result'] == -2
+			puts "[!] Virustotal limits exceeded, ***do not edit the time out values.***"
+		else
+			fres = Hash.new
+			fres['hash'] = file
+			fres['scanner'] = '-'
+			fres['version'] = '-'
+			fres['date'] = '-'
+			fres['result'] = "File failed to upload"
+			
+			results.push fres
+		end
+	rescue Exception => e		
+		puts e.message
+		STDERR.puts "[!] An error has occured. Retrying #{file}\n"
+		sleep $timeout #So we do not DOS virustotal.com we wait 5 seconds between each query
 		retry
 	end
 	
@@ -136,7 +248,15 @@ opt = OptionParser.new { |opt|
   
   opt.on('-s HASH', '--search-hash HASH', 'Searches a single hash on virus total') { |hash| 
     hashes.push(hash)
-  } 
+  }
+
+  opt.on('-u FILE', '--upload-file FILE', 'Uploads a file and waits for results') { |file| 
+    uploads.push(file)
+  }  
+
+  opt.on('-w SITE', '--web-site SITE', 'Searches for a single url on virustotal') { |site| 
+    sites.push(site)
+  }  
 
 	opt.on('-d', '--debug', 'Print verbose debug information') {
 		$options["debug"] = true
@@ -174,10 +294,24 @@ begin
   
   if !hashes.empty?
     hashes.each { |hash|
-      result = fetch_results_from_hash(hash)
+      result = rest_fetch_results_from_hash(hash)
       display_output(result)
     }
   end
+
+	if !uploads.empty?
+		uploads.each do |file|
+			result = rest_upload_and_fetch_results_from_file(file)
+			display_output(result)
+		end
+	end
+
+	if !sites.empty?
+		sites.each do |site|
+			result = rest_fetch_results_from_url(site)
+			display_output(result)
+		end
+	end
   
   if $options["xml"] == true
     puts "</results>"
