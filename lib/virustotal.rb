89ad61534e3462f8597b6b0f729b3a86c87b73fb
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-05 19:21:32 -0600

    Started the library

---
+++ lib/virustotal.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+module VirusTotal
+	APP_NAME = "virustotal"
+	VERSION = "2.0.0"
+	CONFIG_FILE = "~/.virustotal"
+end
+
+require 'virustotal/virustotal'
+require 'virustotal/virustotalresult'
