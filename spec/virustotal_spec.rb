ae23fe258ed5cee7f2160b9ccdd0eb7afec2324b
Jacob Hammack <jacob.hammack@hammackj.com>
2011-01-05 19:21:43 -0600

    Changed the spec to work

--- spec/virustotal_spec.rb
+++ spec/virustotal_spec.rb
@@ -3,11 +3,11 @@ $:.unshift(File.join(File.dirname(base), '../lib'))
 
 module VirusTotal
   describe "virustotal.rb -s FD287794107630FA3116800E617466A9" do
-    it "returns 38 results for FD287794107630FA3116800E617466A9" do
+    it "returns 39 results for FD287794107630FA3116800E617466A9" do
       cmd = IO.popen "./virustotal.rb -s FD287794107630FA3116800E617466A9"
       data = cmd.readlines
       cmd.close
-      data.length.should == 38
+      data.length.should == 39
  			sleep 6
     end
   end
