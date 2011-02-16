c848b80c66095989ac5761e30af4f3d69fb2ba69
Jacob Hammack <jacob.hammack@hammackj.com>
2011-02-15 21:42:36 -0600

    Added all the command line option stuff

--- lib/virustotal/application.rb
+++ lib/virustotal/application.rb
@@ -1,91 +1,124 @@
 
 module VirusTotal
 	class Application
-		def initialize(args)
+		
+		#
+		#
+		#
+		def initialize
 			@options = {}
 			@hashes = Array.new
 			@files = Array.new
 			@sites = Array.new
 		end
 		
-		def parse_options(args)			
-			opt = OptionParser.new do |opt|
-				opt.banner =	"#{APP_NAME} v#{VERSION}\nJacob Hammack\nhttp://www.hammackj.com\n\n"
-				opt.banner << "Usage: #{APP_NAME} <options>"
-				opt.separator('')
-				opt.separator("Search Options")
+		#
+		#
+		#
+		def parse_options(args)
+			begin
+				opt = OptionParser.new do |opt|
+					opt.banner =	"#{APP_NAME} v#{VERSION}\nJacob Hammack\nhttp://www.hammackj.com\n\n"
+					opt.banner << "Usage: #{APP_NAME} <options>"
+					opt.separator('')
+					opt.separator("Search Options")
 				
-				opt.on('-h HASH', '--search-hash HASH', 'Searches a single hash on virustotal.com') { |hash| 
-					@hashes.push(hash)
-				}
+					opt.on('-h HASH', '--search-hash HASH', 'Searches a single hash on virustotal.com') { |hash| 
+						@hashes.push(hash)
+					}
 
-				opt.on('-f FILE', '--search-file FILE', 'Searches a file of hashes on virustotal.com') { |file|
-					if File.exists?(file)
-						puts "[+] Adding file #{file}" unless @options["debug"] != true
-						@files.push(file)
-					else
-						puts "[!] #{file} does not exist, please check your input!\n"
-					end
-				}	 
+					opt.on('-f FILE', '--search-file FILE', 'Searches a file of hashes on virustotal.com') { |file|
+						if File.exists?(file)
+							puts "[+] Adding file #{file}" unless @options["debug"] != true
+							@files.push(file)
+						else
+							puts "[!] #{file} does not exist, please check your input!\n"
+						end
+					}	 
 
-				opt.on('-s SITE', '--search-site SITE', 'Searches for a single url on virustotal.com') { |site| 
-					@sites.push(site)
-				}			
+					opt.on('-s SITE', '--search-site SITE', 'Searches for a single url on virustotal.com') { |site| 
+						@sites.push(site)
+					}			
 						
-				opt.separator('')
-				opt.separator('Output Options')
+					opt.separator('')
+					opt.separator('Output Options')
 
-				opt.on('-x', '--xml-output', 'Print results as xml to stdout') {	
-					@options["xml"] = true
-				}
+					opt.on('-x', '--xml-output', 'Print results as xml to stdout') {	
+						@options["xml"] = true
+					}
 			
-				opt.on('-y', '--yaml-output', 'Print results as yaml to stdout') {
-					@options['yaml'] = true
-				}
+					opt.on('-y', '--yaml-output', 'Print results as yaml to stdout') {
+						@options['yaml'] = true
+					}
 
-				opt.separator ''
-				opt.separator 'Advanced Options'
+					opt.separator ''
+					opt.separator 'Advanced Options'
 
-				opt.on('-c', '--create-config', 'Creates a skeleton config file to use') do					
+					opt.on('-c', '--create-config', 'Creates a skeleton config file to use') do					
 						if File.exists?(File.expand_path(CONFIG_FILE)) == false
 							File.open(File.expand_path(CONFIG_FILE), 'w+') do |f| 
 								f.write("virustotal: \n\tapi-key: \n\ttimeout: \n\n") 
 							end
 
-							puts "[*] An empty #{CONFIG_FILE} has been created. Please edit and fill in the correct values."
+							puts "[*] An empty #{File.expand_path(CONFIG_FILE)} has been created. Please edit and fill in the correct values."
 							exit
 						else
-							puts "[!]  #{CONFIG_FILE} already exists. Please delete it if you wish to re-create it."
+							puts "[!]  #{File.expand_path(CONFIG_FILE)} already exists. Please delete it if you wish to re-create it."
 							exit
 						end
-				end
+					end
 
-				opt.on('-d', '--debug', 'Print verbose debug information') {
-					@options["debug"] = true
-				}
+					opt.on('-d', '--debug', 'Print verbose debug information') do
+						@options["debug"] = true
+					end
 				
-				opt.separator ''
-				opt.separator 'Other Options'
+					opt.separator ''
+					opt.separator 'Other Options'
 				
-				opt.on('-v', '--version', "Shows application version information") do
-					puts "#{APP_NAME} - #{VERSION}"
-					exit
-				end
+					opt.on('-v', '--version', "Shows application version information") do
+						puts "#{APP_NAME} - #{VERSION}"
+						exit
+					end
 
-				opt.on_tail("-?", "--help", "Show this message") { |help|
-					puts opt.to_s + "\n"
-					exit
-				} 
+					opt.on_tail("-?", "--help", "Show this message") { |help|
+						puts opt.to_s + "\n"
+						exit
+					} 
+				end
+							
+			  if ARGV.length != 0 
+			    opt.parse!
+			  else
+			    puts opt.to_s + "\n"
+				  exit
+				end		
+			rescue OptionParser::MissingArgument => m
+				puts opt.to_s + "\n"
+				exit
+			end
+		end
+		
+		#
+		#
+		#
+		def run(args)
+			parse_options(args)
+			
+			if @hashes != nil
+				@hashes.each do |hash|
+				end
+			end
+
+			if @files != nil
+				@files.each do |file|
+				end
 			end
 			
-			opts.parse!
-			options			
-		end
-		
-		def run(args)
-			options = parse_options(args)
-		end
-		
-		
+			if @sites != nil
+				@sites.each do |site|
+				end
+			end
+			
+		end		
 	end
 end
