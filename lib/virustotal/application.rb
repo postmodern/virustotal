f93b45a45a055ab86bc0181c69f5ed51d9780bb4
Jacob Hammack <jacob.hammack@hammackj.com>
2011-02-18 18:01:10 -0600

    Almost working!

--- lib/virustotal/application.rb
+++ lib/virustotal/application.rb
@@ -7,9 +7,11 @@ module VirusTotal
 		#
 		def initialize
 			@options = {}
+			@config = {}
 			@hashes = Array.new
-			@files = Array.new
+			@files_of_hashes = Array.new
 			@sites = Array.new
+			@uploads = Array.new
 		end
 		
 		#
@@ -17,6 +19,9 @@ module VirusTotal
 		#
 		def parse_options(args)
 			begin
+				@options['output'] = :stdout
+				@options['debug'] = false
+				
 				opt = OptionParser.new do |opt|
 					opt.banner =	"#{APP_NAME} v#{VERSION}\nJacob Hammack\nhttp://www.hammackj.com\n\n"
 					opt.banner << "Usage: #{APP_NAME} <options>"
@@ -27,14 +32,23 @@ module VirusTotal
 						@hashes.push(hash)
 					}
 
-					opt.on('-f FILE', '--search-file FILE', 'Searches a file of hashes on virustotal.com') { |file|
+					opt.on('-f FILE', '--search-file FILE', 'Searches a each hash in a file of hashes on virustotal.com') { |file|
 						if File.exists?(file)
-							puts "[+] Adding file #{file}" unless @options["debug"] != true
-							@files.push(file)
+							puts "[+] Adding file #{file}" if @options["debug"]
+							@files_of_hashes.push(file)
 						else
 							puts "[!] #{file} does not exist, please check your input!\n"
 						end
-					}	 
+					}
+					
+					opt.on('-u FILE', '--upload-file FILE', 'Uploads a file to virustotal.com for analysis') do |file|
+						if File.exists?(file)
+							puts "[+] Adding file #{file}" if @options["debug"]
+							@uploads.push(file)
+						else
+							puts "[!] #{file} does not exist, please check your input!\n"
+						end
+					end
 
 					opt.on('-s SITE', '--search-site SITE', 'Searches for a single url on virustotal.com') { |site| 
 						@sites.push(site)
@@ -44,11 +58,15 @@ module VirusTotal
 					opt.separator('Output Options')
 
 					opt.on('-x', '--xml-output', 'Print results as xml to stdout') {	
-						@options["xml"] = true
+						@options["output"] = :xml
 					}
 			
 					opt.on('-y', '--yaml-output', 'Print results as yaml to stdout') {
-						@options['yaml'] = true
+						@options['output'] = :yaml
+					}
+					
+					opt.on('--stdout-output', 'Print results as normal text line to stdout, this is default') {
+						@options['output'] = :stdout
 					}
 
 					opt.separator ''
@@ -57,7 +75,7 @@ module VirusTotal
 					opt.on('-c', '--create-config', 'Creates a skeleton config file to use') do					
 						if File.exists?(File.expand_path(CONFIG_FILE)) == false
 							File.open(File.expand_path(CONFIG_FILE), 'w+') do |f| 
-								f.write("virustotal: \n\tapi-key: \n\ttimeout: 10\n\n") 
+								f.write("virustotal: \n  api-key: \n  timeout: 10\n\n") 
 							end
 
 							puts "[*] An empty #{File.expand_path(CONFIG_FILE)} has been created. Please edit and fill in the correct values."
@@ -68,8 +86,8 @@ module VirusTotal
 						end
 					end
 
-					opt.on('-d', '--debug', 'Print verbose debug information') do
-						@options["debug"] = true
+					opt.on('-d', '--debug', 'Print verbose debug information') do |d|
+						@options["debug"] = d
 					end
 				
 					opt.separator ''
@@ -103,40 +121,65 @@ module VirusTotal
 		#
 		def load_config
 			if File.exists?(File.expand_path(CONFIG_FILE))
-				config = YAML.load_file File.expand_path(CONFIG_FILE)
-				
-				puts config.class
+				@config = YAML.load_file File.expand_path(CONFIG_FILE)
 			else
 				puts "[!] #{CONFIG_FILE} does not exist. Please run virustotal --create-config, to create it."
 				exit
 			end
-			
-			config
 		end
 		
-		#
-		#
+		# 
 		#
 		def run(args)
-			parse_options(args)
-			
+			parse_options(args)		
 			load_config
 			
+			vt = VirusTotal.new(@config["virustotal"]["api-key"], @config["virustotal"]["timeout"], @options["debug"])
+			
+			if @options['output'] == :stdout
+				output_method = :to_stdout
+			elsif @options['output'] == :yaml
+				output_method = :to_yaml
+			elsif @options['output'] == :xml
+				output_method = :to_xml
+				print "<results>\n"
+			end
+						
+			if @files_of_hashes != nil
+				@files_of_hashes.each do |file|
+					f = File.open(file, 'r')
+
+				  f.each do |hash|
+				  	hash.chomp!
+				    @hashes.push hash
+				  end
+				end
+			end		
+						
 			if @hashes != nil
 				@hashes.each do |hash|
-				end
-			end
-
-			if @files != nil
-				@files.each do |file|
+					result = vt.query_hash hash					
+					print result.send output_method
 				end
 			end
 			
 			if @sites != nil
 				@sites.each do |site|
+					result = vt.query_site site
+					print result.send output_method
 				end
 			end
 			
+			if @uploads != nil
+				@uploads.each do |upload|
+					result = vt.query_upload upload
+					print result.send output_method
+				end
+			end
+			
+			if @options['output'] == :xml
+				print "</results>\n"
+			end
 		end		
 	end
 end
