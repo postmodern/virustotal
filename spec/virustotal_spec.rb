0a86c001c14bb3347da7361e2c4d32ab938564fb
Postmodern <postmodern.mod3@gmail.com>
2015-05-28 16:22:02 -0400

    Updated specs so they pass with current virustotal data.

--- spec/virustotal_spec.rb
+++ spec/virustotal_spec.rb
@@ -3,21 +3,21 @@ $:.unshift(File.join(File.dirname(base), '../lib'))
 
 module VirusTotal
   describe "./bin/virustotal -h FD287794107630FA3116800E617466A9" do
-    it "returns 39 results for FD287794107630FA3116800E617466A9" do
+    it "returns 47 results for FD287794107630FA3116800E617466A9" do
       cmd = IO.popen "./bin/virustotal -h FD287794107630FA3116800E617466A9"
       data = cmd.readlines
       cmd.close
-      data.length.should == 38
+      data.length.should == 47
  			sleep 7
     end
   end
   
   describe "./bin/virustotal -s 'http://www.google.com'" do
-    it "returns 6 sites for 'http://www.google.com'" do
+    it "returns 63 sites for 'http://www.google.com'" do
       cmd = IO.popen "./bin/virustotal -s \"http://www.google.com\""
       data = cmd.readlines
       cmd.close      
-      data.length.should == 13
+      data.length.should == 63
 			sleep 7
     end
   end
