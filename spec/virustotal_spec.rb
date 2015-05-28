358d9432b379c001fd93668569d198ac034a82a1
Postmodern <postmodern.mod3@gmail.com>
2015-05-28 16:25:16 -0400

    DRY

--- spec/virustotal_spec.rb
+++ spec/virustotal_spec.rb
@@ -1,24 +1,28 @@
 base = __FILE__
 $:.unshift(File.join(File.dirname(base), '../lib'))
 
-module VirusTotal
-  describe "./bin/virustotal -h FD287794107630FA3116800E617466A9" do
+describe "./bin/virustotal" do
+  def run(arguments)
+    cmd = IO.popen "./bin/virustotal #{arguments}"
+    data = cmd.readlines
+    cmd.close
+    yield data
+    sleep 7
+  end
+
+  context "with -h FD287794107630FA3116800E617466A9" do
     it "returns 47 results for FD287794107630FA3116800E617466A9" do
-      cmd = IO.popen "./bin/virustotal -h FD287794107630FA3116800E617466A9"
-      data = cmd.readlines
-      cmd.close
-      data.length.should == 47
- 			sleep 7
+      run "-h FD287794107630FA3116800E617466A9" do |data|
+        data.length.should == 47
+      end
     end
   end
   
-  describe "./bin/virustotal -s 'http://www.google.com'" do
+  context "with -s 'http://www.google.com'" do
     it "returns 63 sites for 'http://www.google.com'" do
-      cmd = IO.popen "./bin/virustotal -s \"http://www.google.com\""
-      data = cmd.readlines
-      cmd.close      
-      data.length.should == 63
-			sleep 7
+      run "-s http://www.google.com" do |data|
+        data.length.should == 63
+      end
     end
   end
 end
